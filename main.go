b9ef4f96e0a88ee4865a05c5b0489d8ab52e1e11
Mikelangelon <mikelangelon@gmail.com>
2022-06-28 22:11:56 +0200

    Adding a field & submit as a form to write on a json

--- main.go
+++ main.go
@@ -1,13 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"io/ioutil"
+	"os"
 )
+
+type todo struct {
+	Task string
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Awesome TODO list")
 	w.Resize(fyne.NewSize(400, 500))
+
+	res, err := ioutil.ReadFile("todos.json")
+	if err != nil {
+		handleError(err)
+	}
+
+	var myTodos []todo
+	json.Unmarshal(res, &myTodos)
+
+	task := widget.NewEntry()
+	task.SetPlaceHolder("Write task...")
+
+	submit := widget.NewButton("Submit", func() {
+		if task.Text != "" {
+			t := todo{
+				Task: task.Text,
+			}
+			myTodos = append(myTodos, t)
+			b, err := json.MarshalIndent(myTodos, "", " ")
+			if err != nil {
+				handleError(err)
+			}
+			err = os.WriteFile("todos.json", b, 0644)
+			if err != nil {
+				handleError(err)
+			}
+
+			task.Text = ""
+			task.Refresh()
+		}
+	})
+	w.SetContent(container.NewVBox(task, submit))
+
 	w.ShowAndRun()
+}
+
+func handleError(err error) {
+	panic(err)
 }
